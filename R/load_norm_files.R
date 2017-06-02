0223964c38b2d98cf70816bf971398acd3e8f744
Sophia Li <sophia.li0921@gmail.com>
2017-06-02 18:11:18 -0400

    update

--- R/load_norm_files.R
+++ R/load_norm_files.R
@@ -19,6 +19,27 @@ import_codon_count_data <- function(codoncount_data, levels_sample) {
 # group_by(condition) %>% summarise(seqcounts = sum(codon_count_sum)) return(output) }
 
 
-# Check the total count number from each sample that are mapped to coding regions
-# ------------------------------------------------ normalize_raw_data <- function(){ print('hello! World!') }
+# This function takes codon count data and normalize it to RPM, RPKM and TPM -----------------------------------------
+normalize_seqdata <- function(codoncount_data){
 
+    output <- codoncount_data %>% group_by(sample) %>% mutate(RPM = codon_count_sum/sum(codon_count_sum)*10^6) %>%
+        group_by(sample,gene_name) %>%
+        mutate(RPKM = RPM/max(codon_index)*10^3, TPM = codon_count_sum/max(codon_index)*10^3) %>%
+        group_by(sample) %>%
+        mutate(TPM = TPM/sum(TPM)*10^6) %>%
+        group_by(sample,gene_name) %>%
+        mutate(RPM_normgene = RPM/sum(RPM),
+               RPKM_normgene = RPKM/sum(RPKM),
+               TPM_normgene = TPM/sum(TPM)) %>%
+        select(-codon_count_sum,-contains("position"))
+    return(output)
+}
+
+
+# This function sums up all the counts within one gene in each sample -----------------------------------------
+#Nest gene and sample first. It will just sum up the data
+summarize_seqdata <- function(codoncount_data){
+    output_summary <- codoncount_data %>%
+        summarise(gene_len = n(),RPM = sum(RPM),RPKM = sum(RPKM), TPM = sum(TPM))
+    return(output_summary)
+}
